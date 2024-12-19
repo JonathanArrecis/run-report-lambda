96b89087c6c771b2d7a98ced6ff66d615a9f6a89
jonathan <earrecis@nexabanco.com>
2024-12-19 09:46:10 -0600

    delete rds sdk

--- src/main/java/helloworld/config/DatabaseConfig.java
+++ src/main/java/helloworld/config/DatabaseConfig.java
@@ -1,11 +1,5 @@
 package helloworld.config;
 
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.rdsdata.RdsDataClient;
-import software.amazon.awssdk.services.rdsdata.model.ExecuteStatementRequest;
-import software.amazon.awssdk.services.rdsdata.model.ExecuteStatementResponse;
-import software.amazon.awssdk.services.rdsdata.model.Field;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -31,34 +25,12 @@ public class DatabaseConfig {
     public static Connection getConnection() throws RuntimeException{
         try {
             System.out.println("Creating connection to the database");
-            String token = createAuthToken();
-            System.out.println("Connection to the database created");
-            return DriverManager.getConnection(CONNECTION_STRING, System.getenv("DBUSERNAME"), token);
+            String username = System.getenv("DBUSERNAME");
+            String pass = System.getenv("DBPASSWORD");
+            return DriverManager.getConnection(CONNECTION_STRING,username, pass);
         } catch (SQLException e) {
             throw new RuntimeException("Error connecting to the database", e);
         }
     }
 
-    public static String createAuthToken(){
-        System.out.println("Creating RDS IAM Authentication token");
-        RdsDataClient rdsDataClient = RdsDataClient.builder()
-                .region(Region.of(System.getenv("REGION")))
-                .credentialsProvider(DefaultCredentialsProvider.create())
-                .build();
-
-        System.out.println("RDS IAM Authentication token created");
-        ExecuteStatementRequest request = ExecuteStatementRequest.builder()
-                .resourceArn(System.getenv("HOSTNAME"))
-                .secretArn(System.getenv("DBUSERNAME"))
-                .database(System.getenv("DBNAME"))
-                .sql("SELECT 'RDS IAM Authentication'")
-                .build();
-
-        System.out.println("Executing SQL statement");
-        ExecuteStatementResponse response = rdsDataClient.executeStatement(request);
-        System.out.println("SQL statement executed");
-        Field token = response.records().get(0).get(0);
-
-        return token.stringValue();
-    }
 }
